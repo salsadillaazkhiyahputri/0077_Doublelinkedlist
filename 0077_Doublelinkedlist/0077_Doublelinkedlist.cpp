7d873a769d99acb64dab245ed1b58c2018a5b306
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-03 15:03:24 +0700

    Baris kode tersebut digunakan untuk melintasi daftar berantai utk menemukan posisi untuk menyisipkan node baru dengan mengupdate pointer previous menjadi node saat ini dan menggerakkan pointer current ke node berikutnya.

--- 0077_Doublelinkedlist/0077_Doublelinkedlist.cpp
+++ 0077_Doublelinkedlist/0077_Doublelinkedlist.cpp
@@ -42,6 +42,12 @@ void addNode()
 		// insert the new node in the middle or at the end
 		Node* current = START; //step 1.a: start from the first node
 		Node* previous = NULL; //step 1.b: previous node is NULL intially
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{								// step 1.c: traverse the list to find the correct position
+			previous = current;			//step 1.d: move the previous to the current node 
+			current = current->next;	// step 1.e: move the current to the next node
+		}
 	}
 	
 }
